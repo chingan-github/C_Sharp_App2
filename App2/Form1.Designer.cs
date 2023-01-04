fa8ed6fcd82f7283f09ed9051a1c6f1b170c22f6
Chingan <chingan.hsieh@gmail.com>
2023-01-05 01:23:02 +0800

    第一支分支的訊息

--- App2/Form1.Designer.cs
+++ App2/Form1.Designer.cs
@@ -41,7 +41,7 @@ namespace App2
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -49,9 +49,9 @@ namespace App2
             // 
             this.button2.Location = new System.Drawing.Point(49, 78);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(184, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Change Lable";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -67,7 +67,7 @@ namespace App2
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(158, 88);
+            this.label1.Location = new System.Drawing.Point(249, 83);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 12);
             this.label1.TabIndex = 3;
